fbdaffe51da4570129ce937dbf3d1bcd70668c83
yaseenHashiCorp <85969516+yaseenHashiCorp@users.noreply.github.com>
2022-11-03 13:55:49 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "web_instance_ip" {
-value = module.aws-child.web_instance_ip
+value = module.ApprovedInstance.web_instance_ip
 }
 
 output "web_instance_public_dns"{
-value = module.aws-child.web_instance_public_dns
+value = module.ApprovedInstance.web_instance_public_dns
 }
