7db125b9046d2a70e4a1247c6f55ee081631456f
yaseenHashiCorp <85969516+yaseenHashiCorp@users.noreply.github.com>
2022-11-03 13:32:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 
+    cloud {
+    organization = "yaseen-tfcorg"
+    workspaces {
+      tags = [ "   " ]
+    }
+  }
+  }
+
+
+module "aws-child" {
+source = "app.terraform.io/yaseen-tfcorg/child/aws"
+version = "1.0.1"
+region = var.region
+}
+
+output "web_instance_ip" {
+value = module.aws-child.web_instance_ip
+}
+
+output "web_instance_public_dns"{
+value = module.aws-child.web_instance_public_dns
+}
